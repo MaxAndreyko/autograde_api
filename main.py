5fadf04ece57b2372639db3f4bb6e314028a3153
MaxAndreyko <max_andreyko@mail.ru>
2024-01-11 16:47:17 +0300

    add model downloading from s3, add test get response

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import yaml
-from typing import Dict
+from typing import Dict, AnyStr
 
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from utils import dict_to_df, form_send_request_body
 from scorers.bert_rate import K1ScoreRegressor
+from load import s3_load_folder
 
-
+# Read config yaml file
 with open("config.yaml") as cfg:
     cfg_dict = yaml.safe_load(cfg)
 
+# Load BERT model weights for K1-criterion prediction
+s3_load_folder(cfg_dict["s3_data"]["bucket_name"], cfg_dict["s3_data"]["bert_model_path"], cfg_dict["s3_data"]["bert_model_path"])
+
 # Create instances of global classes
 app = FastAPI() # FastAPI
-k1_model = K1ScoreRegressor(**cfg_dict["bert_model"]) # K2-criterion (BERT) model
+k1_model = K1ScoreRegressor(**cfg_dict["bert_model"]) # K1-criterion (BERT) model
 
 class PredictionRequest(BaseModel):
     """Input data model for prediction"""
     data: Dict
+
+@app.get('/', response_model=AnyStr)
+def root():
+    return 'Successfully connected!'
 
 @app.post("/predict")
 async def predict(request: PredictionRequest) -> Dict:
