0ed4b4ffe35eee49c23bd4bb2ace54754261e081
MaxAndreyko <max_andreyko@mail.ru>
2024-01-14 20:40:14 +0300

    refactor code style

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ from app import app
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8080")), log_config=None)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', "8080")), log_config=None)
