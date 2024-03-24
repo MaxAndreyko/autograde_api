63df2978cc7c283daea136f09dd4770014cee3b5
MaxAndreyko <max_andreyko@mail.ru>
2024-03-24 21:05:56 +0300

    move scorers folder to autograde_api folder

---
+++ autograde_api/scorers/k2_scorer.py
@@ -0,0 +1,28 @@
+import re
+
+
+def count_keywords(s: str, keywords: list):
+    count = 0
+    for kw in keywords:
+        res = re.search(kw, s)
+        if res is not None:
+            count += 1
+    return count
+
+
+def count_newlines(s: str):
+    s = re.sub(r"\n+", "\n", s)
+    s = re.sub(r"(.?\n\s?)+", "\n", s).strip("\n")
+    return len(re.findall("\n", s))
+
+
+def get_k2_score(text: str, keywords: list) -> int:
+    kw_count = count_keywords(text, keywords)  # Количество ключевых слов
+    nlines_count = count_newlines(text)  # Количество абзацев (новых строк)
+
+    if (nlines_count >= 6) and (nlines_count < 9) and (kw_count < 6):
+        return 2
+    if (nlines_count >= 9) and (kw_count <= 5):
+        return 0
+    else:
+        return 1
