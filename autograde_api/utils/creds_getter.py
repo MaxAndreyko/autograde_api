00b74671b3fe3325d9112ec549589c36cc680243
MaxAndreyko <max_andreyko@mail.ru>
2024-03-24 21:00:38 +0300

    add credentials getter module

---
+++ autograde_api/utils/creds_getter.py
@@ -0,0 +1,24 @@
+import logging
+from os import environ
+
+from dotenv import find_dotenv, load_dotenv
+
+from autograde_api.utils.helpers import get_none_keys
+
+load_dotenv(find_dotenv())
+
+
+def get_smtp_credentials():
+    logger = logging.getLogger(__name__)
+    smtp_creds = {
+        "smtp_server": environ.get("SMTP_SERVER"),
+        "smtp_port": environ.get("SMTP_PORT"),
+        "smtp_username": environ.get("SMTP_USERNAME"),
+        "smtp_password": environ.get("SMTP_PASSWORD"),
+    }
+    if None in smtp_creds.values():
+        none_keys = get_none_keys(smtp_creds)
+        logger.error(f"SMTP credentials are not set: {none_keys}")
+        return None
+    else:
+        return smtp_creds
