114d1b13e538bc1c6c45aa9c97bd3a25420d7c09
Maxim Andreyko <max_andreyko@mail.ru>
2025-05-14 19:45:11 +0300

    add essay submission data model, modify prediction result and user register data models

--- autograde_api/models/data.py
+++ autograde_api/models/data.py
@@ -1,6 +1,5 @@
-from typing import Dict
-
-from pydantic import BaseModel
+from typing import Dict, Optional
+from pydantic import BaseModel, Field
 
 
 class PredictionRequest(BaseModel):
@@ -11,18 +10,26 @@ class PredictionRequest(BaseModel):
 
 class PredictionResult(BaseModel):
     """Output data model for prediction result"""
-
-    total: int
-    k1_score: int
-    k2_score: int
-    k3_score: int
-    comments: str
-
+    username: str
+    task: str
+    essay: str
+    k1: int
+    k2: int
+    k3: int
+    comments: Optional[str] = Field(default=None, description="Optional comment")
+    
+class EssaySubmission(BaseModel):
+    id: int
+    input_task: str
+    input_essay: str
+    score_content: int
+    score_organization: int
+    score_grammar: int
+    comment: str
+    submitted_at: str
 
 class UserRegister(BaseModel):
     """User data model"""
-
     username: str
     password: str
-    prediction_request: str
-    prediction_result: str
+
