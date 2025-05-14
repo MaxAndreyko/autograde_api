d4df1d7b74b5a5511485a430a4b5aeebec6c1bd6
Maxim Andreyko <max_andreyko@mail.ru>
2025-05-14 19:46:29 +0300

    add reading database credentials

--- autograde_api/database/connection.py
+++ autograde_api/database/connection.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+from autograde_api.utils.creds_getter import get_db_creds
+
+db_creds = get_db_creds()
 
 # Database configuration
-DATABASE_URL = "mysql+asyncmy://essay_user:UserSecurePass456!@localhost:3306/essay_scoring"
+DATABASE_URL = f"mysql+asyncmy://{db_creds['user']}:{db_creds['password']}@{db_creds['host']}:{db_creds['port']}/{db_creds['db_name']}"
 engine = create_async_engine(DATABASE_URL, echo=False)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
